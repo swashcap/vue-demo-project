784efebceff432554a4e61efc45df879280f2ada
Cory Reed <swashcap@gmail.com>
2018-09-24 13:39:17 -0700

    feat: integrate vue-router

--- src/Home.vue
+++ src/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
@@ -15,14 +15,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,19 @@
 import Vue from 'vue'
+import VueRouter from 'vue-router'
+
+import App from './App.vue'
 import Home from './Home.vue'
 
 Vue.config.productionTip = false
+Vue.use(VueRouter)
+
+const router = new VueRouter({
+  routes: [
+    { path: '/', component: Home }
+  ]
+})
 
 new Vue({
-  render: h => h(Home)
+  render: h => h(App),
+  router
 }).$mount('#app')
